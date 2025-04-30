f6ca2cbb5a8c609dcbc75824a983d16b0c9b4cd6
niya-s <niya.somkerd@gmail.com>
2025-04-30 16:40:43 +0700

    refactor: using ngFor and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
@@ -7,7 +9,7 @@ import { TaskComponent } from './task/task.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TaskComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
