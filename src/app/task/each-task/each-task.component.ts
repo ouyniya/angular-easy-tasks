f49a89af8c96938ae60dd5e7387c0a503fabe35a
niya-s <niya.somkerd@gmail.com>
2025-04-30 17:08:39 +0700

    feat: outputting task data in the task components

--- src/app/task/each-task/each-task.component.ts
+++ src/app/task/each-task/each-task.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface Task {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
 
 @Component({
   selector: 'app-each-task',
   standalone: true,
   imports: [],
   templateUrl: './each-task.component.html',
-  styleUrl: './each-task.component.css'
+  styleUrl: './each-task.component.css',
 })
 export class EachTaskComponent {
-
+  @Input({ required: true }) task!: Task;
 }
