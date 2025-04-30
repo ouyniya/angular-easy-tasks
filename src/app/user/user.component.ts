8a7528beabc58ab4c2697a0d94fc108b6a37bfb2
niya-s <niya.somkerd@gmail.com>
2025-04-30 17:16:40 +0700

    feat: dynamic css styling with class binding

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -18,6 +18,7 @@ import { type User } from './user.model';
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter(); // output
 
   get imagePath() {
