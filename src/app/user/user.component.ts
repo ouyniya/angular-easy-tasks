9cc18040f0747d3b1ac38e58f4ad683ae96f4f31
niya-s <niya.somkerd@gmail.com>
2025-04-30 09:12:28 +0700

    introducing signal

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users'; // users array
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,15 +10,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex]; // add attribute to user component
+  selectedUser = signal(DUMMY_USERS[randomIndex]); // add attribute to user component
 
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
+
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
   onSelectUser() {
     // console.log('Clicked !!')
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length); // recalculate every time we click
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    // this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
