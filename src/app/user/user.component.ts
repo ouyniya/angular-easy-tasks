313078b8c93006f0c30dda1ed97b70a8c4a3a650
niya-s <niya.somkerd@gmail.com>
2025-04-30 09:00:29 +0700

    feat: managing state and changing data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users' // users array
+import { DUMMY_USERS } from '../dummy-users'; // users array
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex] // add attribute to user component
+  selectedUser = DUMMY_USERS[randomIndex]; // add attribute to user component
 
   get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    // console.log('Clicked !!')
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length); // recalculate every time we click
+    this.selectedUser = DUMMY_USERS[randomIndex];
   }
 }
