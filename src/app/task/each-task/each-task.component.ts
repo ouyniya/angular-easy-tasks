7035867abd046a5bf8dbebf4a348f98976eab50c
niya-s <niya.somkerd@gmail.com>
2025-05-01 08:15:56 +0700

    style: transform template data with pipes

--- src/app/task/each-task/each-task.component.ts
+++ src/app/task/each-task/each-task.component.ts
@@ -1,20 +1,22 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DatePipe } from '@angular/common';
+// config: https://angular.dev/api/common/DatePipe
 
 import { type Task } from './each-task.model';
-import { CardComponent } from "../../shared/card/card.component";
+import { CardComponent } from '../../shared/card/card.component';
 
 @Component({
   selector: 'app-each-task',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
   templateUrl: './each-task.component.html',
   styleUrl: './each-task.component.css',
 })
 export class EachTaskComponent {
   @Input({ required: true }) task!: Task;
-  @Output() complete = new EventEmitter<string>()
-  
+  @Output() complete = new EventEmitter<string>();
+
   onCompleteTask() {
-    this.complete.emit(this.task.id)
+    this.complete.emit(this.task.id);
   }
 }
