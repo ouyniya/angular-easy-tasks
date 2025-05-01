f7a27c17d21ab5e84b866478f4435c69f3981af7
niya-s <niya.somkerd@gmail.com>
2025-05-01 08:00:52 +0700

    style: content projection with ng-content

--- src/app/task/each-task/each-task.component.ts
+++ src/app/task/each-task/each-task.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { type Task } from './each-task.model';
+import { CardComponent } from "../../shared/card/card.component";
 
 @Component({
   selector: 'app-each-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './each-task.component.html',
   styleUrl: './each-task.component.css',
 })
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,12 +9,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 // // *** option 2
 import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [CardComponent],
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
